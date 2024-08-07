f22426776909c87d9409b3c680530b5da3aaa045
ljw <jwoong1109@naver.com>
2024-08-07 17:47:37 +0900

    feat : Save changes before switching branck

---
+++ src/main/java/com/bangIt/blended/domain/entity/Role.java
@@ -0,0 +1,18 @@
+package com.bangIt.blended.domain.entity;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+	ADMIN("관리자")
+	,PARTNER("판매자")
+	,USER("사용자")
+	;
+	
+	private final String roleName;
+	
+	//getter메서드임
+	public final String roleName() {
+		return roleName;
+	}
+}
---
+++ src/main/java/com/bangIt/blended/security/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.bangIt.blended.security;
+
+public class SecurityConfig {
+
+}
