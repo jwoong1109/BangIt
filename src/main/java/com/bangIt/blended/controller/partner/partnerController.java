9c218013a7cc8084240807b54b3287540f5095a3
gon <lolaven@naver.com>
2024-08-07 14:14:01 +0900

    feat:place

--- src/main/java/com/bangIt/blended/controller/partner/partnerController.java
+++ src/main/java/com/bangIt/blended/controller/partner/partnerController.java
@@ -12,4 +12,23 @@ public class partnerController {
 	public String partner() {
 		return "views/partner/partnerIndex";
 	}
+	
+	//숙소등록
+	@GetMapping("placeSave")
+	public String placeSave() {
+		return "views/partner/place/placeSave";
+	}
+	
+	//숙소목록
+	@GetMapping("placeList")
+	public String placeList() {
+		return "views/partner/place/placeList";
+	}
+	
+	//숙소목록
+	@GetMapping("reservation")
+	public String reservation() {
+		return "views/partner/reservation/reservation";
+	}
+	
 }
