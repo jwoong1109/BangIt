ca43ecfc449aae014ca395266ce7c9bedc552317
gon <lolaven@naver.com>
2024-08-09 13:05:36 +0900

    feat :

--- src/main/java/com/bangIt/blended/controller/partner/partnerController.java
+++ src/main/java/com/bangIt/blended/controller/partner/partnerController.java
@@ -5,32 +5,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class partnerController {
+public class PartnerController {
 	
 	@GetMapping("/partner")
 	public String partner() {
 		return "views/partner/partnerIndex";
 	}
 	
-	//숙소등록
-	@GetMapping("placeSave")
+	//숙소등록 페이지이동
+	@GetMapping("/partner/placeSave")
 	public String placeSave() {
 		return "views/partner/place/placeSave";
 	}
 	
 	//숙소목록
-	@GetMapping("placeList")
+	@GetMapping("partner/placeList")
 	public String placeList() {
 		return "views/partner/place/placeList";
 	}
 	
 	//숙소목록
-	@GetMapping("reservation")
+	@GetMapping("partner/reservation")
 	public String reservation() {
 		return "views/partner/reservation/reservation";
 	}
 	
-	@GetMapping("/roomSave")
+	@GetMapping("partner/roomSave")
     public String roomSave() {
         return "/views/partner/room/roomSave"; // 동일한 템플릿을 반환합니다.
     }
