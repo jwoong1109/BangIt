302b0a22fdc08e915086c3c3372f7774f0823681
ljw <jwoong1109@naver.com>
2024-08-12 12:31:53 +0900

    feat : security Logout

--- src/main/java/com/bangIt/blended/controller/partner/partnerController.java
+++ src/main/java/com/bangIt/blended/controller/partner/partnerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-public class partnerController {
+public class PartnerController {
 	
 	@GetMapping("/partner")
 	public String partner() {
