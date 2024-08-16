c3ca2225cb9e757f9e3a2fe2ebd57cbe1d76d080
gon <lolaven@naver.com>
2024-08-16 10:49:26 +0900

    feat : placeSave

--- src/main/java/com/bangIt/blended/common/util/FileUploadUtil.java
+++ src/main/java/com/bangIt/blended/common/util/FileUploadUtil.java
@@ -7,25 +7,29 @@ import org.springframework.web.multipart.MultipartFile;
 import lombok.RequiredArgsConstructor;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CopyObjectRequest;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class FileUploadUtil {
-
     private final S3Client s3Client;
 
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
+
     @Value("${spring.cloud.aws.s3.upload-temp}")
     private String temp;
+
     @Value("${spring.cloud.aws.s3.upload-src}")
     private String upload;
 
@@ -59,5 +63,30 @@ public class FileUploadUtil {
     private String newFileName(String orgFileName) {
         int index = orgFileName.lastIndexOf(".");
         return UUID.randomUUID().toString() + orgFileName.substring(index);
+    }
+
+    public List<String> s3TempToImages(List<String> tempKeys) {
+        List<String> uploadKeys = new ArrayList<>();
+        tempKeys.forEach(tempKey -> {
+            String[] str = tempKey.split("/");
+            String destinationKey = upload + str[str.length - 1];
+
+            CopyObjectRequest copyObjectRequest = CopyObjectRequest.builder()
+                    .sourceBucket(bucket)
+                    .sourceKey(tempKey)
+                    .destinationBucket(bucket)
+                    .destinationKey(destinationKey)
+                    .build();
+
+            s3Client.copyObject(copyObjectRequest);
+            s3Client.deleteObject(builder -> builder.bucket(bucket).key(tempKey));
+
+            String url = s3Client.utilities()
+                    .getUrl(builder -> builder.bucket(bucket).key(destinationKey))
+                    .toString().substring(6);
+            uploadKeys.add(url);
+        });
+
+        return uploadKeys;
     }
 }
