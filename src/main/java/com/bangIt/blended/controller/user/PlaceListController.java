13bddc6e20ca9e4dbc07de577b70a0514697b253
songol <thdgnl200@gmail.com>
2024-08-19 08:43:47 +0900

    feat:숙소 검색기능 , 크롤링 데이터 얻어오기 진행중

--- src/main/java/com/bangIt/blended/controller/user/PlaceListController.java
+++ src/main/java/com/bangIt/blended/controller/user/PlaceListController.java
@@ -1,14 +1,21 @@
 package com.bangIt.blended.controller.user;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bangIt.blended.domain.dto.placesList.SearchPlaceDTO;
 import com.bangIt.blended.service.user.PlaceListService;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
+
 
 @RequiredArgsConstructor
 @Controller
@@ -16,22 +23,28 @@ public class PlaceListController {
 	
 	private final PlaceListService placeListService;
 	
-	@PostMapping("/search/places")
-	public String postMethodName(SearchPlaceDTO DTO,Model model) {
-		
-		placeListService.findPlaceProcess(DTO,model);
-		
-		
-		return "/views/user/placeList/placelist";
-	}
-	
-	
-	
-	
-	@GetMapping("/placeslist")
-	public String getMethodName() {
-		return "/views/user/placeList/placelist";
-	}
+    @PostMapping("/search/places")
+    public String searchPlaces(SearchPlaceDTO dto, Model model) {
+        placeListService.findPlaceProcess(dto, model);
+        return "/views/user/placeList/placelist";
+    }
+    
+    
+    @ResponseBody
+    @GetMapping("/search/places/scrape/{placeName}")
+    public ResponseEntity<String> scrapePlace(@PathVariable(name = "placeName") String placeName, Model model) {
+    	
+        try {
+            placeListService.scrapeProcess(placeName, model);
+            return ResponseEntity.ok("Scraping completed");
+        } catch (Exception e) {
+          
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error during scraping");
+        }
+    }
+    
+    
+
 	
 
 }
--- src/main/java/com/bangIt/blended/service/user/PlaceListService.java
+++ src/main/java/com/bangIt/blended/service/user/PlaceListService.java
@@ -1,5 +1,7 @@
 package com.bangIt.blended.service.user;
 
+import java.util.List;
+
 import org.springframework.ui.Model;
 
 import com.bangIt.blended.domain.dto.placesList.SearchPlaceDTO;
@@ -7,5 +9,9 @@ import com.bangIt.blended.domain.dto.placesList.SearchPlaceDTO;
 public interface PlaceListService {
 
 	void findPlaceProcess(SearchPlaceDTO dTO, Model model);
+
+	void applyFilters(SearchPlaceDTO dto, List<String> accommodationTypes, List<String> themes, Model model);
+
+	void scrapeProcess(String placeName, Model model);
 
 }
