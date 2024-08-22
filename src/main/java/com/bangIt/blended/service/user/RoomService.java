328dee25e801d54efc63e2fc0c628424bc73d175
gon <lolaven@naver.com>
2024-08-22 13:06:30 +0900

    feat 보완보완

--- src/main/java/com/bangIt/blended/service/user/RoomService.java
+++ src/main/java/com/bangIt/blended/service/user/RoomService.java
@@ -2,10 +2,14 @@ package com.bangIt.blended.service.user;
 
 import org.springframework.ui.Model;
 
+import com.bangIt.blended.domain.dto.room.RoomDetailDTO;
+
 public interface RoomService{
 
 	void roomDetailProcess(long roomId, Model model);
 
 	void listRoomsPlace(long placeId, Model model);
+
+	RoomDetailDTO getRoomDetailById(long roomId);
 
 }
